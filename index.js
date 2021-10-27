624a68f796e710a5ebc083420b7e56ecda7ec269
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-10-27 22:14:21 +0200

    add dot

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3011;
 
-app.get('/', (req, res) => res.send('Hello world'));
+app.get('/', (req, res) => res.send('Hello world.'));
 
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
